35b69cdf60cf4df6f3095e786349c689e3cf5ee1
Vibhav04 <vibhav.sinha@hotmail.com>
2022-07-16 20:56:22 +0530

    first commit

---
+++ lib/categories_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import './category_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(
+              title: catData.title,
+              color: catData.color,
+            ),
+          )
+          .toList(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: const Center(
+        child: Text('Navigation Time!'),
+      ),
+    );
+  }
+}
